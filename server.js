a2262b516ef3967a856305edf3d4661295a32f9a
Tanvir <tanvirmern@gmail.com>
2025-06-19 15:20:50 +0600

    routes

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const cookieParser = require('cookie-parser');
-const express = require('express');
+const cookieParser = require("cookie-parser");
+const express = require("express");
 const port = 3000;
 const app = express();
-const db = require('./config/mongoose-connection');
+const db = require("./config/mongoose-connection");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cookieParser());
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-
+app.use("/owners", require("./routes/owner-routes"));
+app.use("/users", require("./routes/user-routes"));
+app.use("/products", require("./routes/product-routes"));
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
